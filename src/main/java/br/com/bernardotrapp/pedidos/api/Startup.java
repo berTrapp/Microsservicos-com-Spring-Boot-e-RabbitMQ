8a1af582e4c828988a193ba319d266f21aeeb903
berTrapp <bernardo.trappp@gmail.com>
2024-11-26 17:55:04 -0300

    criarPedido

--- src/main/java/br/com/bernardotrapp/pedidos/api/Startup.java
+++ src/main/java/br/com/bernardotrapp/pedidos/api/Startup.java
@@ -3,7 +3,7 @@ package br.com.bernardotrapp.pedidos.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"controller"})
 public class Startup {
 
 	public static void main(String[] args) {
---
+++ src/main/java/enums/Status.java
@@ -0,0 +1,5 @@
+package enums;
+
+public enum Status {
+    EM_PROCESSAMENTO, PROCESSADO;
+}
