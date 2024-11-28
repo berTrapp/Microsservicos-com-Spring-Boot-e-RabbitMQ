e84e46d967869dda65208989ed61744d30916d59
berTrapp <bernardo.trappp@gmail.com>
2024-11-27 21:35:47 -0300

    conclusão da documentação

--- src/main/java/br/com/bernardotrapp/pedidos/api/Startup.java
+++ src/main/java/br/com/bernardotrapp/pedidos/api/Startup.java
@@ -3,7 +3,7 @@ package br.com.bernardotrapp.pedidos.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controller"})
+@SpringBootApplication(scanBasePackages = {"br.com.bernardotrapp.pedidos.api", "config", "controller"})
 public class Startup {
 
 	public static void main(String[] args) {
--- src/main/java/controller/PedidoController.java
+++ src/main/java/controller/PedidoController.java
@@ -2,17 +2,33 @@ package controller;
 
 
 import entity.Pedido;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Pedidos", description = "Recurso para criar um novo pedido")
 @RestController
 @RequestMapping("/v1/pedidos")
 public class PedidoController {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+
+    @Operation(
+            summary = "Cria um novo pedido",
+            description = "Cria um novo pedido no sistema",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Pedido criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class))),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos")
+            }
+    )
 
     @PostMapping("/criarPedido")
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido){
